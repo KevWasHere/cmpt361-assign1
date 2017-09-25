34de7df41ac761965e0966f5b79152acec17e58a
hok37 <hok37@mymacewan.ca>
2017-09-24 19:59:28 -0600

    Added initial addrinfo struct for hint (Storage size error exist)

--- qotdd.c
+++ qotdd.c
@@ -6,8 +6,32 @@
 *-----------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <signal.h>
 
-int main(){
-  printf("Hello, World\n");
+//Have main on bottom. Break up code later
+//http://beej.us/guide/bgnet/output/html/multipage/index.html
+
+int main() {
+  //Standard port if no port is specified
+  char *port = "1700";
+  
+  struct addrinfo hints, *servinfo;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int err = getaddrinfo(NULL, port, &hints, &servinfo);
+  if (err != 0){
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(err));
+    return 1; //Return anything but 0 means error. Might change later
+  }
+
   return 0;
 }
