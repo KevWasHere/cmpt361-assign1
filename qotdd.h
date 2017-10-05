681c11c85cf6c06440b85eefdb22c4ca46f473cb
Kevin Ho <hok37@mymacewan.ca>
2017-10-04 23:17:23 -0600

    Added qotdd's header file

---
+++ qotdd.h
@@ -0,0 +1,12 @@
+#ifndef QOTDD_H_
+#define QOTDD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgen.h>
+
+#include "qotdd_server.h"
+
+/*Prototypes*/
+void usage(char *progname);
+#endif
